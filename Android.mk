e5bd985fc4750656953ce0e0ddcb4e9fd316a936
GaziBoySid <gazi.boy.sid@gmail.com>
2023-12-29 23:58:45 +0530

    configuring Android.mk

--- Android.mk
+++ Android.mk
@@ -1,1 +1,11 @@
 #rock
+
+#Defining LOCAL_PATH variable
+LOCAL_PATH := $(call my-dir)
+
+#gaurding from lunch 
+ifeq ($(TARGET_DEVICE),rock)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
+
+endif
